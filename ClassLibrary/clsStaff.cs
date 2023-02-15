2a351343da7215b01baf82d35e2dfd40418319a8
P2645143 <P2645143@CLPX0027WIN023W>
2023-02-15 19:44:22 +0000

    Create a test for each property in the test class.

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public DateTime Date { get; set; }
+        public int StaffId { get; set; }
+        public string FullName { get; set; }
+        public decimal Salary { get; set; }
+        public string Role { get; set; }
     }
 }
