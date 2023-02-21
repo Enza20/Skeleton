a2461a5011378363ae4823d289f618074da1f9e6
p2668442 <p2668442@GWHXXX6FWIN001W>
2023-02-21 15:45:58 +0000

    Added testing for the customer class

---
+++ Testing2/tstCustomer.cs
@@ -0,0 +1,39 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomer
+    {
+  
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+
+            clsCustomer Customer = new clsCustomer();
+
+            //test to see that it exists
+            Assert.IsNotNull(Customer);
+
+        }
+
+        [TestMethod]
+
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer Customer = new clsCustomer();
+            //create some test data
+            Boolean TestData = true;
+            //assign the data to the propery
+            Customer.Active = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(Customer.Active, TestData);
+
+        }
+    }
+}
+
