b86bcf02249ff16897e9cf7aa71d877a3f4db299
p2668442 <p2668442@GWHXXX6FWIN001W>
2023-02-22 00:39:17 +0000

    Created a test for each customer property in the test class

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
-    {
+    { 
+        public bool IsAllowed { get; set; }
+        public DateTime DateOfRegistration { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerFullName { get; set; }
+        public string CustomerPhoneNumber { get; set; }
+        public string CustomerBillingAddress { get; set; }
+       
     }
 }
