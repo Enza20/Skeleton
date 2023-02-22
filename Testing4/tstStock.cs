a33ed5710901dee7b7b8f8f0461ed7e7d2823983
Daniel Dabrowski <ti1b.daniel.dabrowski@gmail.com>
2023-02-22 13:52:00 +0000

    All testing units finished, HTML layer created

--- Testing4/tstStock.cs
+++ Testing4/tstStock.cs
@@ -26,5 +26,82 @@ namespace Testing4
 
             Assert.AreEqual(Stock.Active, TestData);
         }
+
+        public void ProductIDOK()
+        {
+            clsStock Stock = new clsStock();
+
+            Int32 TestData = 1;
+
+            Stock.ProductID = TestData;
+
+            Assert.AreEqual(Stock.ProductID, TestData);
+        }
+
+        public void DescriptionOK()
+        {
+            clsStock Stock = new clsStock();
+
+            string TestData = "ok";
+
+            Stock.Description = TestData;
+
+            Assert.AreEqual(Stock.Description, TestData);
+        }
+
+        public void QuantityOK()
+        {
+            clsStock Stock = new clsStock();
+
+            Int32 TestData = 1;
+
+            Stock.Quantity = TestData;
+
+            Assert.AreEqual(Stock.Quantity, TestData);
+        }
+
+        public void SupplierIDOK()
+        {
+            clsStock Stock = new clsStock();
+
+            Int32 TestData = 1;
+
+            Stock.SupplierID = TestData;
+
+            Assert.AreEqual(Stock.SupplierID, TestData);
+        }
+
+        public void ExpiryDateOK()
+        {
+            clsStock Stock = new clsStock();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            Stock.ExpiryDate = TestData;
+
+            Assert.AreEqual(Stock.ExpiryDate, TestData);
+        }
+
+        public void ProductPriceOK()
+        {
+            clsStock Stock = new clsStock();
+
+            double TestData = 1;
+
+            Stock.ProductPrice = TestData;
+
+            Assert.AreEqual(Stock.ProductPrice, TestData);
+        }
+
+        public void CanPurchaseOK()
+        {
+            clsStock Stock = new clsStock();
+
+            Boolean TestData = true;
+
+            Stock.CanPurchase = TestData;
+
+            Assert.AreEqual(Stock.CanPurchase, TestData);
+        }
     }
 }
