6e8173041a09b0c4ee7f4a4367d3de09a98e3ea1
p2668442 <p2668442@GWHXXX6FWIN001W>
2023-03-15 13:48:36 +0000

    Updated the find method

--- Testing2/tstCustomer.cs
+++ Testing2/tstCustomer.cs
@@ -6,9 +6,7 @@ namespace Testing2
 {
     [TestClass]
     public class tstCustomer
-    {
-        //private data member for the customer no property
-        private Int32 mCustomerId;
+    {       
         [TestMethod]
         public void InstanceOK()
         {
@@ -19,21 +17,58 @@ namespace Testing2
             //test to see that it exists
             Assert.IsNotNull(Customer);
 
-        }
-
+        }    
+        
         [TestMethod]
-
-        public void IsAllowedPropertyOK()
+        public void CustomerIdPropertyOK()
         {
             //create an instance of the class we want to create
             clsCustomer Customer = new clsCustomer();
-            //create some test data
-            Boolean TestData = true;
+            //create some test data to assign to the property
+            Int32 TestData = 1;
             //assign the data to the property
-            Customer.IsAllowed = TestData;
-            //test to see if the two values are the same
-            Assert.AreEqual(Customer.IsAllowed, TestData);
+            Customer.CustomerId = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Customer.CustomerId, TestData);
+        }
 
+        [TestMethod]
+        public void CustomerFullNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer Customer = new clsCustomer();
+            //create some test data to assign the property
+            string TestData = "Jane Doe";
+            //assign the data to the property
+            Customer.CustomerFullName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Customer.CustomerFullName, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerPhoneNumberPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer Customer = new clsCustomer();
+            //create some test data to assign the property
+            string TestData = "01858100100";
+            //assign the test data to the property 
+            Customer.CustomerPhoneNumber = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Customer.CustomerPhoneNumber, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerBillingAddressPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer Customer = new clsCustomer();
+            //create some test data to assign to the property
+            string TestData = "10 DMU Road";
+            //assign the data to the property
+            Customer.CustomerBillingAddress = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Customer.CustomerBillingAddress, TestData);
         }
 
         [TestMethod]
@@ -50,59 +85,28 @@ namespace Testing2
         }
 
         [TestMethod]
-        public void CustomerIdPropertyOK()
+
+        public void IsAllowedPropertyOK()
         {
             //create an instance of the class we want to create
             clsCustomer Customer = new clsCustomer();
-            //create some test data to assign to the property
-            Int32 TestData = 1;
+            //create some test data
+            Boolean TestData = true;
             //assign the data to the property
-            Customer.CustomerId = TestData;
-            //test to see that the two values are the same
-            Assert.AreEqual(Customer.CustomerId, TestData);
+            Customer.IsAllowed = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(Customer.IsAllowed, TestData);
+           
+
+            String Error = "";
+
+       //     Error = Customer.IsAllowed(customerId, description, quantity, supplierid, expirydate, price);
+
+            Assert.AreEqual(Error, "");
+
         }
 
 
-        [TestMethod]
-        public void CustomerFullNamePropertyOK()
-        {
-            //create an instance of the class we want to create
-            clsCustomer Customer = new clsCustomer();
-            //create some test data to assign the property
-            string TestData = "Jane Doe";
-            //assign the data to the property
-            Customer.CustomerFullName = TestData;
-            //test to see that the two values are the same
-            Assert.AreEqual(Customer.CustomerFullName, TestData);
-        }
-
-
-        [TestMethod]
-        public void CustomerPhoneNumberPropertyOK()
-        {
-            //create an instance of the class we want to create
-            clsCustomer Customer = new clsCustomer();
-            //create some test data to assign the property
-            string TestData = "01858100100";
-            //assign the test data to the property 
-            Customer.CustomerPhoneNumber = TestData;
-            //test to see that the two values are the same
-            Assert.AreEqual(Customer.CustomerPhoneNumber, TestData);
-        }
-
-
-        [TestMethod]
-        public void CustomerBillingAddressPropertyOK()
-        {
-            //create an instance of the class we want to create
-            clsCustomer Customer = new clsCustomer();
-            //create some test data to assign to the property
-            string TestData = "10 DMU Road";
-            //assign the data to the property
-            Customer.CustomerBillingAddress = TestData;
-            //test to see that the two values are the same
-            Assert.AreEqual(Customer.CustomerBillingAddress, TestData);
-        }
         [TestMethod]
         public void FindMethodOK()
         {
@@ -112,7 +116,7 @@ namespace Testing2
             //boolean variable to store the results of the validation
             Boolean Found = false;
             //create some test data to use with the method
-            Int32 CustomerId = 200503;
+            Int32 CustomerId = 1;
             //invoke the method
             Found = ACustomer.Find(CustomerId);
             //test to see if the result is correct
@@ -150,11 +154,11 @@ namespace Testing2
             //boolean variable to record if data is OK (assume it is)
             Boolean OK = true;
             //create some test data to use with the method
-            Int32 CustomerId = 200503;
+            Int32 CustomerId = 1;
             //invoke the method
             Found = ACustomer.Find(CustomerId);
             //check the property
-            if (ACustomer.CustomerFullName != "Jane Doe")
+            if (ACustomer.CustomerFullName != "Joe Bloggs")
             {
                 OK = false;
             }
@@ -172,7 +176,7 @@ namespace Testing2
             //boolean variable to record if data is OK (assume it is)
             Boolean OK = true;
             //create some test data to use with the method
-            Int32 CustomerId = 200503;
+            Int32 CustomerId = 1;
             //invoke the method
             Found = ACustomer.Find(CustomerId);
             //check the property
@@ -194,7 +198,7 @@ namespace Testing2
             //boolean variable to record if data is OK (assume it is)
             Boolean OK = true;
             //create some test data to use with the method
-            Int32 CustomerId = 200503;
+            Int32 CustomerId = 1;
             //invoke the method
             Found = ACustomer.Find(CustomerId);
             //check the property
@@ -218,7 +222,7 @@ namespace Testing2
             //boolean variable to record if data is OK (assume it is)
             Boolean OK = true;
             //create some test data to use with the method
-            Int32 CustomerId = 200503;
+            Int32 CustomerId = 1;
             //invoke the method
             Found = ACustomer.Find(CustomerId);
             //check the property
@@ -241,7 +245,7 @@ namespace Testing2
             //boolean variable to record if data is OK (assume it is)
             Boolean OK = true;
             //create some test data to use with the method
-            Int32 CustomerId = 200503;
+            Int32 CustomerId = 1;
             //invoke the method
             Found = ACustomer.Find(CustomerId);
             //check the property
